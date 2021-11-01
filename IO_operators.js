f70e6c94988d0a3822ada38419d37fc39641eae8
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-01 23:55:48 +0900

    Day01_02

---
+++ IO_operators.js
@@ -0,0 +1,24 @@
+// 2557
+console.log("Hello World!");
+
+// 1000
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let num = [];
+
+rl.on('line', function(line) {
+  num = line.split(' ');
+  
+  let result = Number(num[0]) + Number(num[1]);
+  console.log(result);
+
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
+
