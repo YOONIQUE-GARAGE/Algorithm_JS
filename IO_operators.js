fee5290b7bda0c2b26f6a632859d2f3eb5f837f9
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-03 19:23:22 +0900

    Day03_01

--- IO_operators.js
+++ IO_operators.js
@@ -37,3 +37,26 @@ rl.on('line', function(line) {
 }).on("close", function(){
   process.exit();
 });
+
+// 10869
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let num = [];
+rl.on('line', function(line) {
+  num = line.split(' ');
+  let firstNum = Number(num[0]);
+    let secondNum = Number(num[1]);
+    
+console.log(firstNum + secondNum);
+console.log(firstNum - secondNum);
+console.log(firstNum * secondNum);
+console.log(Math.floor(firstNum / secondNum));
+console.log(firstNum % secondNum);
+  rl.close();
+}).on("close", function(){
+  process.exit();
+});
+
