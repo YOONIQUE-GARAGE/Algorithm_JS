cd77bb063dc21fb31433e684cdec4f3021239c77
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-06 23:57:36 +0900

    Day06_01

--- if_statement.js
+++ if_statement.js
@@ -1,44 +1,44 @@
-// // 1330
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// let num = [];
-// rl.on('line', function(line) {
-//   num = line.split(' ');
-//   let firstNum = Number(num[0]);
-//   let secondNum = Number(num[1]);
-//   if(firstNum > secondNum){
-//     console.log('>');
-//   }else if(firstNum < secondNum){
-//     console.log('<');
-//   }else if(firstNum === secondNum){
-//     console.log('==');
-//   }
-//     rl.close();
-//   }).on("close", function(){
-//     process.exit();
-//   });
+// 1330
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let num = [];
+rl.on('line', function(line) {
+  num = line.split(' ');
+  let firstNum = Number(num[0]);
+  let secondNum = Number(num[1]);
+  if(firstNum > secondNum){
+    console.log('>');
+  }else if(firstNum < secondNum){
+    console.log('<');
+  }else if(firstNum === secondNum){
+    console.log('==');
+  }
+    rl.close();
+  }).on("close", function(){
+    process.exit();
+  });
 
-// // 2753
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// let year = 0;
-// rl.on('line', function(line) {
-//   year = line;
-//   if( year % 4 === 0 && year % 100 !==0 || year % 400 === 0){
-//     console.log(1);
-//   }else {
-//     console.log(0);
-//   }
-//   rl.close();
-//   }).on("close", function(){
-//     process.exit();
-//   });
+// 2753
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let year = 0;
+rl.on('line', function(line) {
+  year = line;
+  if( year % 4 === 0 && year % 100 !==0 || year % 400 === 0){
+    console.log(1);
+  }else {
+    console.log(0);
+  }
+  rl.close();
+  }).on("close", function(){
+    process.exit();
+  });
 
   // 2884
   const readline = require('readline');
