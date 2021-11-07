2463e9ff0a4b7f97d8e9998e978ebb921ed50d3f
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-07 22:36:42 +0900

    Day07_01

--- for_statement.js
+++ for_statement.js
@@ -17,5 +17,38 @@ rl.on('line', function(line) {
     process.exit();
   });
 
+// 15552
+// sol_01) fs이용.
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let range = Number(input[0]);
+let result = '';
+for(let i = 1; i <= range; i++){
+  let num = input[i].split(' ');
+  result += Number(num[0]) + Number(num[1]) + '\n';
+}
+console.log(result);
+// sol_02) readline이용.
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const numArr = [];
+rl.on('line', function(line) {
+  numArr.push(line);
+}).on("close", function(){
+    let range = Number(numArr[0]);
+    for(let i = 1; i <= range; i++){
+    let num1 = Number(numArr[i].split(' ')[0]);
+    let num2 = Number(numArr[i].split(' ')[1]);
+
+    console.log(num1 + num2);
+  }
+  process.exit();
+});
+
+
+
 
 
