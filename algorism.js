48901dec7d0faee7c2db1f398d0ae08d45352c7c
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-11-27 23:16:17 +0900

    Day21_01

--- algorism.js
+++ algorism.js
@@ -2,18 +2,46 @@
 // num1, num2 -> number
 // num1 === 0, num2 === 0
 
-function modulo(num1, num2) {
-  if(num1 === 0 || num1 === num2){
-    return 0;
-  }else if( num2 === 0) {
-    return 'Error: cannot divide by zero';
+// function modulo(num1, num2) {
+//   if(num1 === 0 || num1 === num2){
+//     return 0;
+//   }else if( num2 === 0) {
+//     return 'Error: cannot divide by zero';
+//   }
+//   while(num1 > num2){
+//     num1 = num1 - num2;
+//   }
+//   return num1;
+// }
+
+// let output = modulo(25, 4);
+// console.log(output); // --> 1
+
+// // 16. islsogram
+// // str -> boolean
+function isIsogram(str) {
+  // 모든 문자를 소문자 혹은 대문자로 변환
+  // 값을 다른 배열에 담고, 배열에 있는거랑 현재가 다른지 확인!
+  str = str.toLowerCase();
+  let arr = str.split('');
+  let result = [arr[0]];
+  
+  for(let i = 1; i < arr.length; i++){
+    for(let el of result){
+      if(el === arr[i]){
+        return false;
+      }
+    }
+    result.push(arr[i]);
   }
-  while(num1 > num2){
-    num1 = num1 - num2;
-  }
-  return num1;
+  return true;
 }
 
-let output = modulo(25, 4);
-console.log(output); // --> 1
+let output = isIsogram('aba');
+console.log(output); // false
 
+output = isIsogram('Dermatoglyphics');
+console.log(output); // true
+
+output = isIsogram('moOse');
+console.log(output); // flase
