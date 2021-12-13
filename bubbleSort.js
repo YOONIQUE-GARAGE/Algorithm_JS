0026df38f0044eb9e4fdc1cf95bd465e3d428f33
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-12-13 22:21:38 +0900

    Day29_bubble_loginOk_timeout

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,38 +1,38 @@
-// 로직ok, 실행초과
-// const bubbleSort = function (arr) {
-//   // TODO: 여기에 코드를 작성합니다.
-//   // 원소를 비교해 크면 자리바꾸기 그걸 arr.length -1 만큼 반복
-//   let tempArr = [...arr];
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     for (let j = 0; j < arr.length; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         console.log(`arr[i]:${arr[j]} / arr[j]: ${arr[j + 1]}`);
-//         let moveNum = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = moveNum;
-//         console.log(`arr: ${arr}`);
-//       }
-//       console.log(`arr: ${arr} / tempArr: ${tempArr}`);
-//     }
-//     if (tempArr === arr) {
-//       return arr;
-//     }
-//   }
-//   return arr;
-// };
-
+로직ok, 실행초과
 const bubbleSort = function (arr) {
-  let changed = false;
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let moveNum = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = moveNum;
-      changed = true;
+  // TODO: 여기에 코드를 작성합니다.
+  // 원소를 비교해 크면 자리바꾸기 그걸 arr.length -1 만큼 반복
+  let tempArr = [...arr];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        console.log(`arr[i]:${arr[j]} / arr[j]: ${arr[j + 1]}`);
+        let moveNum = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = moveNum;
+        console.log(`arr: ${arr}`);
+      }
+      console.log(`arr: ${arr} / tempArr: ${tempArr}`);
+    }
+    if (tempArr === arr) {
+      return arr;
     }
   }
-  return changed ? bubbleSort(arr) : arr;
+  return arr;
 };
 
-let output = bubbleSort([5, 4, 3, 2, 1]);
-console.log(output); // --> [1, 2, 3]
+// const bubbleSort = function (arr) {
+//   let changed = false;
+//   for (let j = 0; j < arr.length; j++) {
+//     if (arr[j] > arr[j + 1]) {
+//       let moveNum = arr[j];
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = moveNum;
+//       changed = true;
+//     }
+//   }
+//   return changed ? bubbleSort(arr) : arr;
+// };
+
+// let output = bubbleSort([5, 4, 3, 2, 1]);
+// console.log(output); // --> [1, 2, 3]
