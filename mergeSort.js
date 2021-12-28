e7390cb7d656e5282179889abfbfd4d71e516466
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2021-12-28 11:22:50 +0900

    Day40_mergeSort_finish

--- mergeSort.js
+++ mergeSort.js
@@ -1,27 +1,32 @@
 const mergeSort = function (arr) {
   // TODO: 여기에 코드를 작성합니다.
-  // 각각의 원소를 하나의 배열로 취급
-  const newArr = arr.map((el) => [el]);
-  console.log(newArr);
-  let result = '';
-  //인접한 2개 병합
-  let count = 1;
-  console.log(Math.round(newArr.length / 2));
-  while(count < Math.round(newArr.length / 2)){
-    console.log(`count-1: ${newArr[count]}`);
-    if(newArr[count -1 ] > newArr [count]){
-      result = [...newArr[count], ...newArr[count - 1]]
+  // mergeSort는 배열을 나눠주는 역할
+  // merge는 배열을 정렬해 주는 역할
+  // BMR
+  if (arr.length === 1) return arr;
+  // 반
+  let middle = Math.floor(arr.length / 2);
+  // 배열을 반으로 나눈다.
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+  console.log(`left: ${left} / right: ${right}`);
+  // 배열을 받아 합쳐주는 함수
+  const merge = function (left, right) {
+    const result = [];
+    // BMR
+    while (left.length !== 0 && right.length !== 0) {
+      if (left[0] <= right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
     }
-    console.log(result);
-    count = count + 2;
-  }
-  if(newArr[count - 1] !== undefined){
-    result = [...result, ...newArr[count -1 ]];
-  }
-  console.log(result);
-  return result;
-};
+    console.log(`mergeResult: ${[...result, ...left, ...right]}`);
+    return [...result, ...left, ...right];
+  };
 
+  return merge(mergeSort(left), mergeSort(right));
+};
 
 let output = mergeSort([5, 4, 3, 2, 1]);
 console.log(output); // --> [1, 2, 3, 4, 5]
