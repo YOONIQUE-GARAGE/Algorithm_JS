f10c280a18a7bd0b76adb74463c69b551baf1860
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2022-01-09 22:40:52 +0900

    Day50_rotateMatrix_1회전_modify

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,22 +1,28 @@
 // (x,y)와 2차원 배열의 차이점
 // 2차원배열은 arr[i][j]이면 세로축기준으로 i만큼 아래 있고,
 // 가로축 기준으로 j만큼 옆에 있는걸 뜻한다.
+// 1번회전시
 const rotateMatrix = function (matrix) {
   // TODO: 여기에 코드를 작성합니다.
   // 90도 회전시 해당 2차원 배열 리턴
-  // 세로(col)
+  // 세로(col) - i
   const N = matrix.length;
-  // 가로(row)
+  // 가로(row) - j
   const M = matrix[0].length;
   let result = [];
-  for (let i = 0; i < N; i++) {
+  // 원래 N * M
+  // 90도 회전시 M * N
+  for (let i = 0; i < M; i++) {
     result[i] = [];
-    for (let j = 0; j < M; j++) {
-      result[i][j] = matrix[M - j - 1][i];
+    for (let j = 0; j < N; j++) {
+      result[i][j] = matrix[N - j - 1][i];
     }
   }
   return result;
 };
+
+// M * N인 2차원 배열 K번 회전시
+// const rotateMatrix = function (matrix) {};
 
 const matrix = [
   [1, 2, 3, 4],
