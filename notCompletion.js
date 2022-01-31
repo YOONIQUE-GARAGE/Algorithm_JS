2cec4f4034a485ca64a5b721128c5d9e834cd844
YOONIQUE-GARAGE <yooniquegarage@gmail.com>
2022-01-31 13:25:35 +0900

    Day71_notCompletion_someFail

---
+++ notCompletion.js
@@ -0,0 +1,11 @@
+// 01. some testcase fail And Speed fail
+//function solution(participant, completion) {
+//  for (let com of completion) {
+//    for (let i = 0; i < participant.length; i++) {
+//      if (com === participant[i]) {
+//        participant.splice(i, 1);
+//      }
+//    }
+//  }
+//  return `${participant}`;
+//}
